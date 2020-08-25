ab2b92ce999b6d7ee190ff6b5ff006465f0fd77e
alprogs <alprogs@gmail.com>
2020-08-25 22:04:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Show summarized statistics information from babyTim App.
+This will be used for MMM-BabyTime of MagicMirror project.
---
+++ src/main/java/babyTimeHelper/App.java
@@ -0,0 +1,284 @@
+package babyTimeHelper;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class App {
+
+	private static Map<String, String> typeTransMap 	= null;
+	private static Date today 	= null;
+	
+	private void init() {
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+
+		typeTransMap 	= new HashMap<>();
+		typeTransMap.put( "snack", "간식" );
+		typeTransMap.put( "weaning", "이유식" );
+		typeTransMap.put( "dried_milk", "분유" );
+
+		today 	= new Date( System.currentTimeMillis() );
+	}
+
+	public void doProcess() {
+		init();
+		
+		String activity_list_data	= reqActivityList();
+		String activity_recent_data	= reqActivityRecent();
+		
+		JsonElement listActivities 	= parseActivitiesData( activity_list_data );
+		calcTodayActivities( listActivities.getAsJsonArray() );
+
+		JsonElement recentActivities 	= parseActivitiesData( activity_recent_data );
+		calcLastFeedingTime( recentActivities.getAsJsonObject() );
+	}
+
+	public String reqActivityListNew() {
+		String messageBody 	= "{\"baby_oid\":\"5d457a1b3bd31d2e176dec0b\",\"size\":50,\"start_align\":true,\"tz\":\"Asia\\/Seoul\"}";
+		String activity_list_url = "https://babytime.simfler.com/v1/activity/list";
+
+		String responseBody 	= "NOT ASSIGNED";
+
+		try {
+			OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+			MediaType mediaType = MediaType.parse("text/plain");
+
+			RequestBody body = RequestBody.create(mediaType, messageBody);
+
+			Request request = new Request.Builder()
+				.url( activity_list_url )
+				.method("POST", body)
+				.addHeader("User-Agent", "Android SM-N960N")
+				.addHeader("bt-pf", "android")
+				.addHeader("bt-pf-ver", "10")
+				.addHeader("bt-app-ver", "308")
+				.addHeader("bt-udid", "6288347c-6c87-4fa0-b261-552cf302db9f")
+				.addHeader("bt-lang", "ko_KR")
+				.addHeader("Host", "babytime.simfler.com")
+				.addHeader("Connection", "Keep-Alive")
+				.addHeader("Accept-Encoding", "gzip")
+				.addHeader("Content-Length", "113")
+				.addHeader("Cookie", "token=\"2|1:0|10:1598182894|5:token|168:eyJfaWQiOiAiNWQ0NTc5YzMxYTlkNzg1MWE0OWQyMDc4IiwgInAiOiAiNTAxM2JlYWU5MzkyZmE3NWNiMGFmNjJmZTk5ZjliZDUxMjA3ODZmNDQ4NTZkMjE2MmE2ZmJkNDIiLCAiZSI6IDI1NDQyNjI4OTQuMDg3NTkxfQ==|de19413ed422859c069891123ab864b871ef6109d4aefc2b853f0f601e153e0b\"; token=\"2|1:0|10:1598358552|5:token|168:eyJfaWQiOiAiNWQ0NTc5YzMxYTlkNzg1MWE0OWQyMDc4IiwgInAiOiAiNTAxM2JlYWU5MzkyZmE3NWNiMGFmNjJmZTk5ZjliZDUxMjA3ODZmNDQ4NTZkMjE2MmE2ZmJkNDIiLCAiZSI6IDI1NDQ0Mzg1NTIuNzM2NTgyOH0=|7b3568cf0e031bc43babd8d6ccbf8b92ee589e2e1862839c88699a55c6a3bd47\"")
+				.addHeader("Content-Type", "text/plain")
+				.build();
+
+			Response response 	= client.newCall(request).execute();
+			responseBody 		= response.body().string();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return responseBody;
+	}
+	
+	public String reqActivityList() {
+		String messageBody 	= "{\"baby_oid\":\"5d457a1b3bd31d2e176dec0b\",\"size\":50,\"start_align\":true,\"tz\":\"Asia\\/Seoul\"}";
+		return sendHttpRequest( "https://babytime.simfler.com/v1/activity/list", messageBody );
+	}
+
+	public String reqActivityRecent() {
+		String messageBody 	= "{\"filters\":[\"feeding\",\"diaper\",\"sleep\",\"pumping\",\"medicine\"],\"baby_oid\":\"5d457a1b3bd31d2e176dec0b\",\"timestamp\":0}";
+
+		return sendHttpRequest( "https://babytime.simfler.com/v1/activity/recent", messageBody );
+	}
+
+	public void calcLastFeedingTime( JsonObject activities ) {
+		JsonObject recentFeeding 	= activities.getAsJsonObject( "feeding" );
+
+		String feedingType 		= recentFeeding.get( "type" ).getAsString();
+		JsonObject feedingOption	= recentFeeding.get( "option" ).getAsJsonObject();
+
+		int feedingAmount	= feedingOption.get( "amount" ).getAsInt();
+
+		long started_at 	= recentFeeding.get("started_at").getAsLong() * 1000;
+		Date activityTime 	= new Date( started_at );
+
+		long timeDiff 		= today.getTime() - activityTime.getTime();
+
+		log(String.format("LAST FEEDING       : %s %s ml", typeTransMap.get( feedingType ), feedingAmount));
+		log(String.format("TIME DIFF          : %s", formatDuration( timeDiff )));
+	}
+
+	private static String formatDuration(long duration) {
+		long hours = TimeUnit.MILLISECONDS.toHours(duration);
+		long minutes = TimeUnit.MILLISECONDS.toMinutes(duration) % 60;
+		//long seconds = TimeUnit.MILLISECONDS.toSeconds(duration) % 60;
+		//long milliseconds = duration % 1000;
+		
+		return String.format("%d 시간 %d 분 전", hours, minutes);
+	}
+
+	private void calcTodayActivities(JsonArray activities) {
+		int totalFeeding 	= 0;
+		int totalDriedMilk 	= 0;
+		int totalWeaning 	= 0;
+
+		JsonArray todayActivities 	= getTodayActivities( activities );
+
+		for (JsonElement activityElem: todayActivities) {
+			JsonObject activity 	= activityElem.getAsJsonObject();
+
+			String type 		= activity.get( "type" ).getAsString();
+			JsonObject option 	= activity.get( "option" ).getAsJsonObject();
+
+			int amount 			= 0;
+			if (option.get( "amount" ) != null) {
+				amount 			= option.get( "amount" ).getAsInt();
+			}
+
+			if (type.equals( "dried_milk" )) {
+				totalDriedMilk 	+= amount;
+
+			} else if (type.equals( "weaning" )) {
+				totalWeaning 	+= amount;
+			}
+		}
+
+		totalFeeding 	= totalDriedMilk + totalWeaning;
+		
+		log("TOTAL FEEDING      : "+ totalFeeding +"( "+ totalDriedMilk +" + "+ totalWeaning +" )");
+	}
+
+	private JsonElement parseActivitiesData( String responseMessage ) {
+		
+		JsonElement activities 	= null;
+
+		try {
+			JsonParser parser 	= new JsonParser();
+			JsonObject root 	= (JsonObject) parser.parse( responseMessage );
+			JsonObject data 	= root.getAsJsonObject( "data" );
+
+			activities 			= data.get( "activities" );
+
+		} catch ( Exception e ) {
+			log( e );
+		}
+
+		return activities;
+	}
+
+	private JsonArray getTodayActivities(JsonArray activities) {
+		JsonArray activities_tmp 	= new JsonArray();
+
+		for (JsonElement activityElem: activities) {
+			JsonObject activity 	= activityElem.getAsJsonObject();
+
+			long started_at 	= activity.get("started_at").getAsLong() * 1000;
+			Date activityTime 	= new Date( started_at );
+
+			long noOfDaysBetween 	= ChronoUnit.DAYS.between( activityTime.toLocalDate(), today.toLocalDate() );
+			
+			if (noOfDaysBetween == 0) {
+				activities_tmp.add( activity );
+			}
+		}
+
+		return activities_tmp;
+	}
+
+	private void printActivity( JsonObject activity ) {
+		log( "==================================================");
+		String type 		= activity.get("type").getAsString();
+
+		JsonObject option 	= activity.getAsJsonObject("option");
+
+		int amount 			= 0;
+		String snackType 	= "";
+		
+		if ( type.equals("dried_milk") || type.equals("weaning") ) {
+			amount 	= option.get("amount").getAsInt();
+		} else if ( type.equals("snack") ) {
+			snackType 	= option.get("snack_type").getAsString();
+		}
+
+		long started_at 	= activity.get("started_at").getAsLong() * 1000;
+
+		Date activityTime 	= new Date( started_at );
+		SimpleDateFormat 	prettyTimeTag 	= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		String info 	= (snackType.length() == 0)? String.valueOf( amount ):snackType;
+
+		log("TYPE: "+ typeTransMap.get( type ));
+		log("INFO: "+ info);
+		log("TIME: "+ prettyTimeTag.format( activityTime ));
+	}
+
+	public String sendHttpRequest(String urlStr, String messageBody){
+		String responseBody 	= "NOT ASSIGNED";
+
+		try {
+			OkHttpClient client = new OkHttpClient().newBuilder().build();
+
+			MediaType mediaType = MediaType.parse("text/plain");
+
+			RequestBody body = RequestBody.create(mediaType, messageBody);
+
+			Request request = new Request.Builder()
+				.url( urlStr )
+				.method("POST", body)
+				.addHeader("User-Agent", "Android SM-N960N")
+				.addHeader("bt-pf", "android")
+				.addHeader("bt-pf-ver", "10")
+				.addHeader("bt-app-ver", "308")
+				.addHeader("bt-udid", "6288347c-6c87-4fa0-b261-552cf302db9f")
+				.addHeader("bt-lang", "ko_KR")
+				.addHeader("Host", "babytime.simfler.com")
+				.addHeader("Connection", "Keep-Alive")
+				.addHeader("Accept-Encoding", "gzip")
+				.addHeader("Content-Length", "113")
+				.addHeader("Cookie", "token=\"2|1:0|10:1598182894|5:token|168:eyJfaWQiOiAiNWQ0NTc5YzMxYTlkNzg1MWE0OWQyMDc4IiwgInAiOiAiNTAxM2JlYWU5MzkyZmE3NWNiMGFmNjJmZTk5ZjliZDUxMjA3ODZmNDQ4NTZkMjE2MmE2ZmJkNDIiLCAiZSI6IDI1NDQyNjI4OTQuMDg3NTkxfQ==|de19413ed422859c069891123ab864b871ef6109d4aefc2b853f0f601e153e0b\"; token=\"2|1:0|10:1598358552|5:token|168:eyJfaWQiOiAiNWQ0NTc5YzMxYTlkNzg1MWE0OWQyMDc4IiwgInAiOiAiNTAxM2JlYWU5MzkyZmE3NWNiMGFmNjJmZTk5ZjliZDUxMjA3ODZmNDQ4NTZkMjE2MmE2ZmJkNDIiLCAiZSI6IDI1NDQ0Mzg1NTIuNzM2NTgyOH0=|7b3568cf0e031bc43babd8d6ccbf8b92ee589e2e1862839c88699a55c6a3bd47\"")
+				.addHeader("Content-Type", "text/plain")
+				.build();
+
+			Response response 	= client.newCall(request).execute();
+			responseBody 		= response.body().string();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return responseBody;
+	}
+
+	private void log(Exception e) {
+		printSTEInfo( Thread.currentThread().getStackTrace()[2], e.getMessage() );
+	}
+
+	private void log(String message) {
+		printSTEInfo( Thread.currentThread().getStackTrace()[2], message );
+	}
+
+	private void printSTEInfo(StackTraceElement ste, String message) {
+		String className 		= ste.getClassName();
+		className 	= className.substring( className.lastIndexOf(".") +1, className.length());
+
+		StringBuilder sb = new StringBuilder();
+		sb.append( String.format("[%-15s][%-15s][%-3s] ", className, ste.getMethodName(), ste.getLineNumber()) );
+		sb.append(message);
+
+		System.out.println( sb.toString() );
+	}
+
+    public static void main(String[] args) {
+		App proc = new App();
+		
+		proc.doProcess();
+	}
+}
+
